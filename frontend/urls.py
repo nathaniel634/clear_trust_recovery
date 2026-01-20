5cb8a3c81a1814c95f5b102ff6c136f99dde73cb
NephygTdkingz <tdkingzict@gmail.com>
2026-01-20 18:16:17 +0100

    done with frontend part

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,4 +5,8 @@ from . import views
 app_name = 'frontend'
 urlpatterns = [
     path('', views.home_view, name="home"),
+    path('about-us/', views.about_view, name="about"),
+    path('services/', views.services_view, name="services"),
+    path('contact-us/', views.contact_view, name="contact"),
+    path('file-complaint/', views.file_complaint_view, name="file_complaint"),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def home_view(request):
     return render(request, 'frontend/index.html')
+
+def about_view(request):
+    return render(request, 'frontend/about.html')
+
+def services_view(request):
+    return render(request, 'frontend/services.html')
+
+def contact_view(request):
+    return render(request, 'frontend/contact.html')
+
+def file_complaint_view(request):
+    return render(request, 'frontend/file_complaint.html')
